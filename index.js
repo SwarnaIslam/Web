df14ac61403560a1d43ba80bb22007aaa6b929d8
swarnaIITDU <swarna-12-2019116857@it.du.ac.bd>
2023-08-13 10:07:57 +0600

    setup for express

--- index.js
+++ index.js
@@ -2,38 +2,32 @@ const http=require('http');
 const fs=require('fs');
 const hostName=process.env.HOSTNAME;
 const port=process.env.PORT;
+function sendHTML(data, res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(data);
+
+    res.end();
+}
 
 const server=http.createServer((req, res)=>{
     if(req.url=='/'){
         fs.readFile('index.html',(err,data)=>{
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-
-            res.end();
+            sendHTML(data,res);
         });
     }
     else if(req.url=='/about'){
         fs.readFile('about.html',(err,data)=>{
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-
-            res.end();
+            sendHTML(data,res);
         });
     }
     else if(req.url=='/contact-me'){
         fs.readFile('contact-me.html',(err,data)=>{
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-
-            res.end();
+            sendHTML(data,res);
         });
     }
     else{
         fs.readFile('404.html',(err,data)=>{
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-
-            res.end();
+            sendHTML(data,res);
         });
     }
 });
